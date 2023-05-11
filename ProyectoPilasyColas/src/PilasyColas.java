5d0564ba5a424af46a88ab8f0e3d2060286869d0
CamilaGiraldoGiraldo <giraldocamila2004@gmail.com>
2023-05-11 18:48:18 -0500

    otra prueba

--- ProyectoPilasyColas/src/PilasyColas.java
+++ ProyectoPilasyColas/src/PilasyColas.java
@@ -3,5 +3,6 @@ import javax.swing.JOptionPane;
 public class PilasyColas {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        JOptionPane.showMessageDialog(null, "Conectados");
     }
 }
