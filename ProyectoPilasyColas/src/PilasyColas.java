ae487a5a72cf60710f7b9c69216910b61975a88c
CarlosOrtizContreras <carlosaoc2611@gmail.com>
2023-05-11 19:15:42 -0500

    ...

--- ProyectoPilasyColas/src/PilasyColas.java
+++ ProyectoPilasyColas/src/PilasyColas.java
@@ -2,15 +2,6 @@ import javax.swing.JOptionPane;
 
 public class PilasyColas {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        JOptionPane.showMessageDialog(null, "Conectados");
-        while(true){
-            boolean var = true;
-            int otro;
-        }
-        while(true){
-            int t = 34;
-            boolean f = false;
-        };
+       
     }
 }
