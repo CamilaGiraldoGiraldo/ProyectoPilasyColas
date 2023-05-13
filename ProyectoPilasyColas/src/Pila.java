92dd2a161cb5265708180c411ad2c1d6c471b6cf
CamilaGiraldoGiraldo <giraldocamila2004@gmail.com>
2023-05-12 19:34:52 -0500

    Creación clase Pila e inserté algunos métodos

---
+++ ProyectoPilasyColas/src/Pila.java
@@ -0,0 +1,61 @@
+public class Pila {
+        //Atributos
+        int Limite, Tope, Vector[];
+
+        public Pila(int n) {
+            this.Limite = n - 1;
+            this.Tope = -1;
+            this.Vector = new int[n];
+        }
+    
+        public void setVector(int[] Vector) {
+            this.Vector = Vector;
+        }
+    
+        public boolean PilaVacia() {
+            if (Tope == -1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    
+        public boolean PilaLLena() {
+            boolean B = false;
+            if (Tope == Limite) {
+                B = true;
+            }
+            return B;
+        }
+    
+        void Apilar(int d) {
+            Tope++;
+            Vector[Tope] = d;
+        }
+    
+        int Desapilar() {
+            int aux = Vector[Tope];
+            Tope--;
+            return aux;
+        }
+    
+        void Pasar_datos(Pila aux) {
+            int Auxiliar;
+            while (aux.PilaVacia() == false) {
+                Auxiliar = aux.Desapilar();
+                Apilar(Auxiliar);
+            }
+        }
+    
+        public String Mostrar(Pila aux) {
+            String s = " ";
+            int Dato = 0;
+            while (PilaVacia() != true) {
+                Dato = Desapilar();
+                s = s + "|" + Dato + "|\n";
+                aux.Apilar(Dato);
+            }
+            Pasar_datos(aux);
+            return s;
+        }
+}
--- ProyectoPilasyColas/src/PilasyColas.java
+++ ProyectoPilasyColas/src/PilasyColas.java
@@ -2,16 +2,45 @@ import javax.swing.JOptionPane;
 
 public class PilasyColas {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        JOptionPane.showMessageDialog(null, "Conectados");
-        while (true) {
-            String bd = "";
+        int n = 0, d,opc;
+        n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el tamaño de la Pila"));
+        Pila P1 = new Pila(n);
+        Pila Aux =new Pila (n);
+        do {
+            opc = Menu();
+            switch (opc) {
+                case 1: 
+                    if (P1.PilaLLena() == false)
+                    {
+                        d = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un valor"));
+                        P1.Apilar(d);
+                    }
+                    else
+                    {
+                        JOptionPane.showMessageDialog(null, "Pila Llena...");
+                    }
+                    break;
+                case 3:
+                    if (P1.PilaVacia() == false)
+                    {
+                        JOptionPane.showMessageDialog(null, P1.Mostrar(Aux));
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "Pila Vacia...");
+                    }
+                    break;
+            }
+            }
+            while (opc != 4);
+        }
 
-            jshdbjdsbjdshhsj
-
-
-
-            hbjhbjbjbjb
+    public static int Menu() {
+        int opc;
+        opc = Integer.parseInt(JOptionPane.showInputDialog("*** Menu principal ***\n\n"
+                + "1.Apilar\n"
+                + "2.Desapilar \n"
+                + "3.Mostrar Pila\n"
+                + "84. Salir"));
+        return opc;
     }
-}
-}
+    }
